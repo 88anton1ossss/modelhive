ffd1d0180e39aa165777f5e8191e587014e912be
88anton1ossss <88.anton1ossss@gmail.com>
2026-03-01 15:02:11 -0700

    fix: fix cookie type error in middleware.ts

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -15,7 +15,7 @@ export async function updateSession(request: NextRequest) {
                     return request.cookies.getAll()
                 },
                 setAll(cookiesToSet) {
-                    cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value, options))
+                    cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
                     supabaseResponse = NextResponse.next({
                         request,
                     })
@@ -28,8 +28,8 @@ export async function updateSession(request: NextRequest) {
     )
 
     // IMPORTANT: Avoid writing any logic between createServerClient and
-    // supabase.auth.getUser(). A simple mistake can make it very hard to debug
-    // auth issues.
+    // supabase.auth.getUser(). A simple mistake could make it very hard to debug
+    // issues with users being randomly logged out.
 
     const {
         data: { user },
@@ -38,21 +38,20 @@ export async function updateSession(request: NextRequest) {
     if (
         !user &&
         !request.nextUrl.pathname.startsWith('/login') &&
-        !request.nextUrl.pathname.startsWith('/auth')
+        !request.nextUrl.pathname.startsWith('/auth') &&
+        !request.nextUrl.pathname.startsWith('/api/webhooks') &&
+        !request.nextUrl.pathname.startsWith('/marketplace') &&
+        !request.nextUrl.pathname.startsWith('/civitai-alternative') &&
+        !request.nextUrl.pathname.startsWith('/lora-marketplace') &&
+        !request.nextUrl.pathname.startsWith('/photo-dataset-marketplace') &&
+        !request.nextUrl.pathname.startsWith('/earn-from-ai-models') &&
+        request.nextUrl.pathname !== '/'
     ) {
         // no user, potentially respond by redirecting the user to the login page
-        // const url = request.nextUrl.clone()
-        // url.pathname = '/login'
-        // return NextResponse.redirect(url)
+        const url = request.nextUrl.clone()
+        url.pathname = '/login'
+        return NextResponse.redirect(url)
     }
-
-    // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
-    // creating a new response object with NextResponse.next() make sure to:
-    // 1. Pass the request in it, like so:
-    //    const myNewResponse = NextResponse.next({ request })
-    // 2. Copy over the cookies, like so:
-    //    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
-    // 3. Change the myNewResponse object to fit your needs, but remember that it has to be returned!
 
     return supabaseResponse
 }
